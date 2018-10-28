c7308bba1aa006e8ead3d78f98029f0097a3563f
Jaap Glasbergen <j.glasbergen@outlook.com>
2018-10-28 14:51:26 +0000

    requirement.txt en 2 bash scripts toegevoegd

--- sensorWorker/management/commands/readSensor.py
+++ sensorWorker/management/commands/readSensor.py
@@ -4,27 +4,28 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(25, GPIO.IN)
+GPIO.setup(26, GPIO.IN)
 GPIO.setup(18, GPIO.OUT)
 
 def my_callback(channel):
-    if GPIO.input(25):     # if port 25 == 1
-        # print("Rising edge detected on 25")
+    if GPIO.input(26):     # if port 25 == 1
+        # print("Rising edge detected on 26")
         GPIO.output(18, GPIO.HIGH)
         Group("sensor").send({'text': "Button pushed"})
-    else:                  # if port 25 != 1
-        # print("Falling edge detected on 25")
+    else:                  # if port 256!= 1
+        # print("Falling edge detected on 26")
         GPIO.output(18, GPIO.LOW)
         Group("sensor").send({'text': "Button released"})
 
 class Command(BaseCommand):
-    help = "Reading sensor on port 25"
+    help = "Reading sensor on port 26"
 
     def handle(self, *args, **kwargs):
-        GPIO.add_event_detect(25, GPIO.BOTH, callback=my_callback)
+        GPIO.add_event_detect(26, GPIO.BOTH, callback=my_callback)
 
         while True:
             input("Press Enter when ready\n>")
+            GPIO.cleanup()
 
 
 
